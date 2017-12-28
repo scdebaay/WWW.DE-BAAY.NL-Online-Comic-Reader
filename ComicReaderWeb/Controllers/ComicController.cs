4546a0776638669b674c6a53a952a28cc2ed8ea4
Solino de Baay <scdebaay@hotmail.com>
2017-12-28 22:21:14 +0100

    Opruimen van een poging om MVC4 naar MVC5 up te graden. Wat een bende.

--- ComicReaderWeb/Controllers/ComicController.cs
+++ ComicReaderWeb/Controllers/ComicController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using AttributeRouting.Web.Http;
-using AttributeRouting;
 using ComicReaderWeb.Models;
 
 
