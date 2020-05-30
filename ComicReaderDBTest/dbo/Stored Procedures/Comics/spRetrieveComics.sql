db9cdc287047c8c5607f56d9a0265776e7975d13
Solino de Baay <scdebaay@hotmail.com>
2020-05-30 18:11:48 +0200

    Rearranged stored procedures into Class folders. Also implemented Search in Comic Retrieval spRetrieveComics.sql

---
+++ ComicReaderDBTest/dbo/Stored Procedures/Comics/spRetrieveComics.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spRetrieveComics]
+	@SearchText NVARCHAR(100)
+AS
+BEGIN
+SELECT * FROM [dbo].[Comics]
+	WHERE [ComicDeleted] = 0 AND @SearchText = '' OR 
+	[ComicDeleted] = 0 AND [Name] LIKE '%'+@SearchText+'%' OR
+	[ComicDeleted] = 0 AND [Path] LIKE '%'+@SearchText+'%'
+	ORDER BY [Path];
+END
--- ComicReaderDBTest/dbo/Stored Procedures/spRetrieveComics.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[spRetrieveComics]
-AS
-SELECT * FROM [dbo].[Comics]
-	WHERE [ComicDeleted] = 0
-	ORDER BY [Path];
