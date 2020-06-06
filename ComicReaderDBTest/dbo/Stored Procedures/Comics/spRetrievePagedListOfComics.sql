ee2295e9852ee58730aa9ee00184c7d0d528ed7b
Solino de Baay <scdebaay@hotmail.com>
2020-06-06 23:33:23 +0200

    Database implementation 2020.06.1.5. Added searchText parameter to retrieve paged comic list .  To be used with the API.

--- ComicReaderDBTest/dbo/Stored Procedures/Comics/spRetrievePagedListOfComics.sql
+++ ComicReaderDBTest/dbo/Stored Procedures/Comics/spRetrievePagedListOfComics.sql
@@ -1,8 +1,11 @@
 ﻿CREATE PROCEDURE [dbo].[spRetrievePagedListOfComics]
 	@pageLimit int,
-	@page int = 0
+	@page int = 0,
+	@SearchText NVARCHAR(100)
 AS
-SELECT * FROM [dbo].[Comics] WHERE [ComicDeleted] = 0
+SELECT * FROM [dbo].[Comics] WHERE [ComicDeleted] = 0 AND @SearchText = '' OR 
+	[ComicDeleted] = 0 AND [Name] LIKE '%'+@SearchText+'%' OR
+	[ComicDeleted] = 0 AND [Path] LIKE '%'+@SearchText+'%'
 	ORDER BY [Path]
 	OFFSET @page*@pageLimit ROWS
 	FETCH NEXT @pageLimit ROWS ONLY;
