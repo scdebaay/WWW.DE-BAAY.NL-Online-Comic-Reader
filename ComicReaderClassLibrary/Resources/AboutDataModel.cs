2deb3b0aa18a71a5621408f6139f4268966c14e8
Solino de Baay <scdebaay@hotmail.com>
2020-06-13 11:49:14 +0200

    ComicReader Class Library, Version 1.0, Release 2020.06.1.6, Added an AboutDataModel to be used by other applications to provide version information in a uniform manner.

---
+++ ComicReaderClassLibrary/Resources/AboutDataModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComicReaderClassLibrary.Resources
+{
+    public class AboutDataModel : IAboutDataModel
+    {
+        private string _apiName;
+        public string ApiName
+        {
+            get
+            {
+                return _apiName;
+            }
+            set
+            {
+                _apiName = value;
+            }
+        }
+
+        private string _apiVersion;
+        public string ApiVersion
+        {
+            get
+            {
+                return _apiVersion;
+            }
+            set
+            {
+                _apiVersion = value;
+            }
+        }
+
+        private string _libraryName;
+        public string LibraryName
+        {
+            get
+            {
+                return _libraryName;
+            }
+            set
+            {
+                _libraryName = value;
+            }
+        }
+
+        private string _libraryVersion;
+        public string LibraryVersion
+        {
+            get
+            {
+                return _libraryVersion;
+            }
+            set
+            {
+                _libraryVersion = value;
+            }
+        }
+
+        private string _apiLocation;
+        public string ApiLocation
+        {
+            get
+            {
+                return _apiLocation;
+            }
+            set
+            {
+                _apiLocation = value;
+            }
+        }
+    }
+}
---
+++ ComicReaderClassLibrary/Resources/IAboutDataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ComicReaderClassLibrary.Resources
+{
+    public interface IAboutDataModel
+    {
+        string ApiLocation { get; set; }
+        string ApiName { get; set; }
+        string ApiVersion { get; set; }
+        string LibraryName { get; set; }
+        string LibraryVersion { get; set; }
+    }
+}
